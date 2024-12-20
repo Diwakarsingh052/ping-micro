28f84b554ad150399979dca04106b23e83163a5d
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-20 15:41:29 +0530

    .

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 
-		w.Write([]byte("hello working"))
+		w.Write([]byte("hello working v1"))
 	})
 	log.Println("Server started")
 	http.ListenAndServe(":80", nil)
